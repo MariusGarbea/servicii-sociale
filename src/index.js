60954c52dbf2eb0e5d3791b7dbfd06ae32bd394d
Cezar Neaga <hi@cezarneaga.eu>
2016-12-16 15:43:54 +0200

    babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
+import 'babel-polyfill';
 import React from 'react';
-// import 'babel-polyfill';
 import { render } from 'react-dom';
 import Routes from './config/routes';
 import { browserHistory } from 'react-router';
